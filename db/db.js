23f43dc3565d70beb8f1ebaabb2dd23539700601
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2022-01-18 12:50:06 +0300

    d2

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ dotenv.config()
 const dbConnection=async()=>{
    
     try {
-        const conn=await mongoose.connect(process.env.mongo)
+        const conn=await mongoose.connect('mongodb+srv://abdulaziz:azizsk123@cluster0.yvmzf.mongodb.net/duladb?retryWrites=true&w=majority')
         console.log(`connected to mongodb`);
     } catch (err) {
         console.log(err);
